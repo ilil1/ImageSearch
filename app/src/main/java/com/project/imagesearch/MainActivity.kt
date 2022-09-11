c7d7e81c0980a815adb5bb88ee4e67f5ebe41b29
k <free.rjs102@gmail.com>
2022-09-12 02:51:50 +0900

    viewpager2 TabLayout 기능 구현

--- app/src/main/java/com/project/imagesearch/MainActivity.kt
+++ app/src/main/java/com/project/imagesearch/MainActivity.kt
@@ -2,10 +2,38 @@ package com.project.imagesearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.project.imagesearch.databinding.ActivityMainBinding
+import com.project.imagesearch.widget.SectionsPagerAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this)
+        val viewPager: ViewPager2 = binding.viewPager
+
+        viewPager.adapter = sectionsPagerAdapter
+
+        val tabs: TabLayout = binding.tabs
+
+        TabLayoutMediator(tabs, viewPager) { tab, position ->
+            tab.text = if (position == 0) {
+                "이미지 검색"
+            }
+            else {
+                "즐겨 찾기"
+            }
+
+        }.attach()
+
     }
 }
