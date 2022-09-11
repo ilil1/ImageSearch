7e30e2202bdbd53876590d63f8486aa950a12d0d
k <free.rjs102@gmail.com>
2022-09-12 02:52:27 +0900

    ImageSearchFragment() 추가

--- app/src/main/java/com/project/imagesearch/widget/SectionsPagerAdapter.kt
+++ app/src/main/java/com/project/imagesearch/widget/SectionsPagerAdapter.kt
@@ -1,13 +1,16 @@
-package com.project.imagesearch.main
+package com.project.imagesearch.widget
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.project.imagesearch.main.ImageSearchFragment
 
 class SectionsPagerAdapter(private val fragmentActivity: FragmentActivity) :
     FragmentStateAdapter(fragmentActivity) {
 
-    override fun createFragment(position: Int): Fragment {}
+    override fun createFragment(position: Int): Fragment {
+        return ImageSearchFragment()
+    }
 
     override fun getItemCount(): Int {
         return 2
