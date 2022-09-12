84b9408ec64ff552a17054d342bca8da394e62a5
k <free.rjs102@gmail.com>
2022-09-13 06:17:32 +0900

    update

--- app/src/main/java/com/project/imagesearch/main/ImageSearchViewModel.kt
+++ app/src/main/java/com/project/imagesearch/main/ImageSearchViewModel.kt
@@ -16,7 +16,8 @@ class ImageSearchViewModel : ViewModel() {
     private val repository = NaverImageSearchRepository()
     private val queryFlow = MutableSharedFlow<String>()
     private val favorites = mutableSetOf<Item>()
-    private val _favoritesFlow = MutableSharedFlow<List<Item>>()
+    //replay
+    private val _favoritesFlow = MutableSharedFlow<List<Item>>(replay = 1)
 
     val pagingDataFlow = queryFlow
         .flatMapLatest {
