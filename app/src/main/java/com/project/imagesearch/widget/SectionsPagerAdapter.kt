3ac3ecf7d41e6a910af12fcef395ee05dba4eb28
k <free.rjs102@gmail.com>
2022-09-13 06:18:24 +0900

    position에 따른 fragment 재생성

--- app/src/main/java/com/project/imagesearch/widget/SectionsPagerAdapter.kt
+++ app/src/main/java/com/project/imagesearch/widget/SectionsPagerAdapter.kt
@@ -3,13 +3,19 @@ package com.project.imagesearch.widget
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.project.imagesearch.main.FavouritesFragment
 import com.project.imagesearch.main.ImageSearchFragment
 
 class SectionsPagerAdapter(private val fragmentActivity: FragmentActivity) :
     FragmentStateAdapter(fragmentActivity) {
 
     override fun createFragment(position: Int): Fragment {
-        return ImageSearchFragment()
+        return if(position == 0) {
+            ImageSearchFragment()
+        } else {
+            FavouritesFragment()
+        }
+
     }
 
     override fun getItemCount(): Int {
